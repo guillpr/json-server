f2a7ecc4f4737aaa8be037f26f7086c625a39d4f
guillaume.proulx <guillaume.proulx@taq.gouv.qc.ca>
2023-06-13 14:03:40 -0400

    ret

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
-server.unableCors({
-  origin='*',
-  methods:'GET,POST'
-})
 
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
