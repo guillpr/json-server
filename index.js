f8adf2d1ca60a2a12df6d7e9b630d32ac7ae8322
guillaume.proulx <guillaume.proulx@taq.gouv.qc.ca>
2023-06-13 13:32:36 -0400

    ajustement server.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 const PORT = 8000
 
